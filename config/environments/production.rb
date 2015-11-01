39fbd32eedcedf1a0fa697337ed08cae7d295aca
Robert Elroy <relroy5499@gmail.com>
2015-10-31 20:07:26 -0500

    try14

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,26 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'relroy5499@gmail.com',
+  password:             'Smitty10',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+  
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
