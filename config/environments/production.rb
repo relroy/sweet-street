4d6dac6b05fec0cae9dd234eda32b90e7224ba20
Robert Elroy <relroy5499@gmail.com>
2015-10-31 20:41:27 -0500

    try17

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'example.com',
-  user_name:            ENV[email],
-  password:             ENV[password],
+  user_name:            ENV["GMAIL_USERNAME"],
+  password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
   
