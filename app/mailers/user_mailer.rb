8f3dfbf1eb79c654531a096b54e28cbf2c95538b
Robert Elroy <relroy5499@gmail.com>
2015-11-03 21:41:15 -0600

    put in signin email config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  # after_create :signin_confirmation
+  after_create :signin_confirmation
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
