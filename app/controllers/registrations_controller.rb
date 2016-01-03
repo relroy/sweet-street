191c5cb0be1a6ac6e51e2c1c26291f08f7179ab1
Robert Elroy <relroy5499@gmail.com>
2016-01-03 13:11:40 -0600

    editing registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  def edit
+    if user_signed_in?
+      @user = User.find(params[:id])
+      else render :file => "/public/404.html", :status => 404
+    end
+  end
  
 end
