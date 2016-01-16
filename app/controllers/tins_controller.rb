ef0d724f0003142a4e85558cad22c568b51c50d4
Robert Elroy <relroy5499@gmail.com>
2016-01-16 15:37:41 -0600

    set up tin controller

--- app/controllers/tins_controller.rb
+++ app/controllers/tins_controller.rb
@@ -1,2 +1,46 @@
 class TinsController < ApplicationController
+   def index
+    @tins = Tin.all
+  end
+
+  def show
+    @tin = Tin.find(params[:id])
+  end
+
+  def new
+    if user_signed_in? && current_user.admin?
+    @tin = Tin.new
+    else render :file => "/public/404.html", :status => 404
+    end
+  end
+
+  def create
+    @tin = Tin.create(item_params) 
+    redirect_to '/tins'  
+  end
+
+  def edit
+    @tin = Tin.find(params[:id])
+  end
+
+  def update
+    @tin = Tin.find(params[:id])
+    @tin.update(item_params)
+    flash[:success] = "Item has been Updated!"
+    redirect_to "/tins"
+  end
+
+  def destroy
+    @tin = Tin.find(params[:id])
+    @tin.destroy
+    flash[:warning] = "Your item has been deleted!"
+    redirect_to "/tins"
+  end
+
+  private
+
+  def item_params
+    return params.require(:tin).permit(:name, :photo, :description, :id)
+
+  end
 end
