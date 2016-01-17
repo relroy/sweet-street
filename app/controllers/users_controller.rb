90a25b9e5fd8e08d12c761f2a88853efb896f4f6
Robert Elroy <relroy5499@gmail.com>
2016-01-17 15:14:07 -0600

    non

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    if user_signed_in?
+    if user_signed_in? && current_user.ws_cust?
       @user = User.find(params[:id])
       else render :file => "/public/404.html", :status => 404
     end
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       @user.update({:ws_cust => params[:ws_cust], :phone => params[:phone],:address => params[:address],:city => params[:city],:state => params[:state],:zip_code => params[:zip_code],:email => params[:email]})
        flash[:info] = "Update Complete"
-      redirect_to "/users"
+      redirect_to "/"
     end  
   end
 
